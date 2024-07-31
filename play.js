2c0027932fe961432bffa563d2ce0facc0162605
MaazGhazi <maaz.ghazi1738@gmail.com>
2024-07-30 20:30:42 -0400

    Continue

--- play.js
+++ play.js
@@ -1,7 +1,17 @@
-const http = require('http');
+const http = require('http'); // imported b/c its used to create a server
 
-const server = http.createServer((req, res) => {
-  console.log(req);
+const server = http.createServer((req, res) => { // creating the server and passing a function that will be executed for every incoming request "req" and response "res"
+  const url = req.url;
+  const method = req.method;
+  
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+}
+
 });
 
 server.listen(3000);
