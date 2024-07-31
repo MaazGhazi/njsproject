df3a07d87bb2ea8cddf87bca453fca89350e5848
MaazGhazi <maaz.ghazi1738@gmail.com>
2024-07-30 23:32:50 -0400

    Updating the commit message

--- play.js
+++ play.js
@@ -1,17 +1,26 @@
-const http = require('http'); // imported b/c its used to create a server
+const http = require('http');
 
-const server = http.createServer((req, res) => { // creating the server and passing a function that will be executed for every incoming request "req" and response "res"
-  const url = req.url;
-  const method = req.method;
-  
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-}
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
 
+
+    if (url === '/message' && method === 'POST') {
+      req.on
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
 });
 
 server.listen(3000);
